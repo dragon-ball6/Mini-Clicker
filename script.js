2df4ea1e4b1b322cfcfa6e10ccb2b57bfa54e788
dragon-ball6 <93067888+dragon-ball6@users.noreply.github.com>
2023-11-18 22:53:23 +0000

    Base
    Just a small part of it :smiley:

---
+++ script.js
@@ -0,0 +1,48 @@
+// base_money is clicks
+// money types = ["white pieces", "new coins", "ancestral dimes", "dimensional gems"];
+
+// init
+{
+let clicks = document.getElementById("cliks");
+let clicker = document.getElementById("click");
+let upgrade = document.getElementById("upgrade");
+
+let monies = {
+	clicks: 0,
+	white_pieces: 0,
+	new_coins: 0,
+	ancestral_dimes: 0,
+	dimensional_gems: 0
+};
+let increase = 1;
+let auto = 0;
+let trade = 0;
+
+
+clicker.addEventListener("mousedown", function() {
+	monies.clicks += increase;
+});
+
+class Upgrade {
+	constructor(type, qty) {
+		this.type = ["click","auto","trade"][type];
+		this.qty = this.type!="trade" ? this.qty : 1;
+	}
+	update() {
+		switch (this.type) {
+			case "click":
+				increase += this.qty;
+			break;
+			case "auto":
+				auto += this.qty;
+			break;
+			case "trade":
+				trade++;
+			break;
+		}
+	}
+	display() {
+		return `+${this.qty} ${this.type}${this.type=="trade" ? "progress" : ""}`;
+	}
+}
+}
